3dc36044f0f3078618921d399bf12f7d10995075
Kiran Shaji <shajikiran369@gmail.com>
2026-04-18 02:41:56 -0700

    clean up duplicate files from merge

--- rtl/p1_design.v
+++ rtl/p1_design.v
@@ -5,16 +5,16 @@ module seq_detector_0011(
     output reg detected
 );
 
-    reg [3:0] shift_reg;
+reg [3:0] history;
 
-    always @(posedge clk) begin
-        if (reset) begin
-            shift_reg <= 4'b0000;
-            detected <= 1'b0;
-        end else begin
-            detected <= (shift_reg == 4'b0011);
-            shift_reg <= {shift_reg[2:0], data_in};
-        end
+always @(posedge clk) begin
+    if (reset) begin
+        history <= 4'b0000;
+        detected <= 1'b0;
+    end else begin
+        detected <= (history == 4'b0011);
+        history <= {history[2:0], data_in};
     end
+end
 
 endmodule
